47dc38c7521931f1cce56f6374cc735654a559b3
marcell-lovassy <marcell.lovassy@gmail.com>
2023-06-26 13:37:53 +0200

    Create Unit Action System
    Implement a system to be able to select and control individual units

--- Assets/Game/Scripts/UnitActionSystem.cs
+++ Assets/Game/Scripts/UnitActionSystem.cs
@@ -1,45 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public event UnityAction OnSelectedUnitChanged;
+
+    public static UnitActionSystem Instance { get; private set; }
+
+    public Unit SelectedUnit => selectedUnit;
 
     [SerializeField]
     private Unit selectedUnit;
     [SerializeField]
     private LayerMask unitLayer;
 
+
+    private void Awake()
+    {
+        if(Instance != null) 
+        {
+            Debug.LogError("There is more than one UnitActionSystem! " + transform + " - " + Instance);
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if(HandleUnitSelection())
-            {
-                return;
-            }
+            if(TryUnitSelection()) return;
             SetUnitMovement();
         }
     }
 
-    private bool HandleUnitSelection()
+    private bool TryUnitSelection()
     {
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if(Physics.Raycast(ray, out RaycastHit hitInfo, float.MaxValue, unitLayer))
         {
             if(hitInfo.transform.TryGetComponent<Unit>(out Unit unit)) 
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
         return false;
     }
 
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke();
+    }
+
     private void SetUnitMovement() 
     {
-        Vector3 target = MouseController.GetPosition();
-        selectedUnit.Move(target);
+        if(selectedUnit != null) 
+        {
+            Vector3 target = MouseController.GetPosition();
+            selectedUnit.Move(target);
+        }
     }
 }
